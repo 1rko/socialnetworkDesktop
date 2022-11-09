53e8c7430008f1be1a8c42f0ee7e74d020e52d41
1rko <117205339+1rko@users.noreply.github.com>
2022-11-09 16:35:03 +0300

    Получение доступа к компоненту через Ref

--- src/Components/Content/Dialogs/Dialogs.js
+++ src/Components/Content/Dialogs/Dialogs.js
@@ -1,34 +1,41 @@
 import React from 'react';
 import styles from './Dialogs.module.css'
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Dialogs = (props) => {
     const dialogsArray = props.dialogs.map(dialogItem => {
         return (
             <div className={styles.item}>
                 <NavLink to={"/dialogs/" + dialogItem.id}
-                         className={navData => navData.isActive ? (styles.active) : styles.unactive}>
+                    className={navData => navData.isActive ? (styles.active) : styles.unactive}>
                     {dialogItem.name}
                 </NavLink>
             </div>
         )
     })
 
-    const messagesArray = props.messages.map(messageItem =>{
-        return(
+    const messagesArray = props.messages.map(messageItem => {
+        return (
             <div className={styles.messageItem}>
                 {messageItem.message}
             </div>
         )
-        }
+    })
 
-    )
+    let newPostElement = React.createRef();
+    let addNewPost = () => {
+        let text = newPostElement.current?.value;
+        alert(text);
+    }
 
     return (
         <div className={styles.dialogs_wrapper}>
             <div className={styles.dialog_items}>
 
                 {dialogsArray}
+
+                <textarea className={styles.dialogsMessage} ref={newPostElement}></textarea>
+                <button className={styles.buttonNewDialogsMessage} onClick={addNewPost}>Добавить новое сообщение</button>
 
             </div>
 
