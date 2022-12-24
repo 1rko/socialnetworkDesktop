cce6e69c53795b1aeacd371184d9ca397645059e
1rko <117205339+1rko@users.noreply.github.com>
2022-12-24 23:10:07 +0300

    - Users сделан как классовый компонет -в нем создан метод componentDidMount, в котором идет запрос на сервер - методы follow/unfollow не работают, т.к. они изменяют state Redux, а у нас данные берутся с сервера

--- src/Components/Content/Users/Users.js
+++ src/Components/Content/Users/Users.js
@@ -1,13 +1,66 @@
 import React from 'react';
 import styles from './Users.module.css'
+import axios from "axios";
 
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            initFromServer: 0
+        }
+    };
+
+    componentDidMount() {
+        axios('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            this.setState({initFromServer: response.data});
+        })
+    }
+
+    render() {
+        let allUsers;
+        if (this.state.initFromServer) {
+            allUsers = this.state.initFromServer.items.map(usersItem => {
+                let imgSrc=(usersItem.photos.small!==null)?
+                    usersItem.photos.small:
+                    'https://bobbyhadz.com/images/blog/javascript-check-if-variable-is-not-null/banner.webp'
+                return (<div className={styles.user_item}>
+                    <img src={imgSrc} className={styles.ava} alt='Аватар'/>:
+                    <div>{usersItem.name}</div>
+                    <div>{usersItem.status}</div>
+                    {usersItem.followed ?
+                        <button onClick={() => {
+                            this.props.onUnFollow(usersItem.id)
+                        }}> 'Unfollow' </button> :
+                        <button onClick={() => {
+                            this.props.onFollow(usersItem.id)
+                        }}>'Follow' </button>
+                    }
+                </div>)
+            })
+        } else allUsers = "Данные не загрузились"
+        return (
+            <div className={styles.users_wrapper}>
+                {allUsers}
+            </div>
+        )
+    }
+}
+
+/*
 const Users = (props) => {
+  let initFromServer=axios('https://social-network.samuraijs.com/api/1.0/users').then(response=>{
+    console.log(response.data)
+  })
+
   const allUsers = props.usersData.map(usersItem => {
     return <div className={styles.user_item}>
-      <img src={usersItem.url} className={styles.ava} />
+      <img src={usersItem.url} className={styles.ava} alt='Аватар'/>
       <div>{usersItem.name}</div>
       <div>{usersItem.age}</div>
       <div>{usersItem.messages}</div>
+      <span>{usersItem.location.country+"  "}</span>
+      <span>{usersItem.location.city}</span>
+
       {usersItem.followed ?
         <button onClick={() => { props.onUnFollow(usersItem.id) }}> 'Unfollow' </button> :
         <button onClick={() => { props.onFollow(usersItem.id) }}>'Follow' </button>
@@ -21,5 +74,7 @@ const Users = (props) => {
     </div>
   );
 }
+
+ */
 
 export default Users;
