c9cae2bb93ebcd511efa2d994c47193fbc41b641
1rko <117205339+1rko@users.noreply.github.com>
2023-02-16 16:45:24 +0300

    Создан пользовательский компонент формы TextArea, который вставляется в форму на странице Profile

--- src/Components/Content/Profile/Posts/Posts.js
+++ src/Components/Content/Profile/Posts/Posts.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import styles from './Posts.module.css'
 import PostItem from "../PostItem/PostItem";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import MyTextArea from '../../../../Common/Controls/TextArea';
 
 const Posts = (props) => {
 
     const postText = props.postData.map(postItem => {
-        return <PostItem likesCount={postItem.likesCount} postText={postItem.postText}/>
+        return <PostItem likesCount={postItem.likesCount} postText={postItem.postText} />
     })
 
     let newPostElement = React.createRef();
 
-    let onAddPost = () => {
-        let newPostText = props.newPostText;
+    let onAddPost = (newPostText) => {
         props.addPost(newPostText)
         props.updateNewPostText('')
     }
@@ -25,18 +26,56 @@ const Posts = (props) => {
 
         <div className={styles.profile_wrapper}>
 
+            <PostForm funcAddPost={onAddPost} />
+
+//Это не нужно
             <textarea
                 className={styles.dialogsMessage}
                 ref={newPostElement}
                 onChange={onPostChange}
                 value={props.newPostText}
-                placeholder='Enter new post text'/>
+                placeholder='Enter new post text' />
             <button className={styles.buttonNewDialogsMessage} onClick={onAddPost}>Добавить новый пост через
                 CAllBack из state
             </button>
+///
             {postText}
         </div>
     );
 }
+
+const PostForm = (props) => (
+    <div>
+        <Formik
+            initialValues={{
+                newPost: ""
+            }}
+            validate={values => {
+                const errors = {};
+                if (!values.newPost) {
+                    errors.newPost = 'Required';
+                }
+                return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    props.funcAddPost(values.newPost);
+                    setSubmitting(false);
+                }, 400);
+            }}
+        >
+            {({ isSubmitting }) => (
+                <Form>
+                    <Field name="newPost" component={MyTextArea} placeholder="Enter new post text" />
+
+                    <button type="submit" disabled={isSubmitting}>
+                        Submit
+                    </button>
+                </Form>
+            )}
+        </Formik>
+    </div>
+);
 
 export default Posts;
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from './Login.module.css'
-import {Formik, Form, Field, ErrorMessage} from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 
 const Login = (props) => {
     return (
         <div>
             <h1>Login</h1>
-            <LoginForm/>
+            <LoginForm />
         </div>
     )
 }
@@ -16,42 +16,53 @@ const validate = values => {
     const errors = {};
     if (!values.login) {
         errors.login = 'Required';
-    } else if (values.login.length > 15) {
-        errors.login = 'Must be 15 characters or less';
     }
-
-    if (!values.password) {
-        errors.password = 'Required';
-    }
-
-    if (!values.email) {
-        errors.email = 'Required';
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = 'Invalid email address';
-    }
-
     return errors;
 };
 
 const LoginForm = () => (
     <div>
         <Formik
-            initialValues={{login: "", password: ""}}
+            initialValues={{
+                login: "", password: "",
+                color: "", firstName: "",
+                secondName: ""
+            }}
             validate={validate}
-            onSubmit={(values, {setSubmitting}) => {
+            onSubmit={(values, { setSubmitting }) => {
                 setTimeout(() => {
                     alert(JSON.stringify(values, null, 2));
                     setSubmitting(false);
                 }, 400);
             }}
         >
-            {({isSubmitting}) => (
+            {({ isSubmitting }) => (
                 <Form>
-                    <Field type="text" name="login" placeholder ='login'/>
+                    <Field type="text" name="login" placeholder='login' />
                     <ErrorMessage name="login" component="div" />
-                    <Field type="password" name="password" placeholder ='password'/>
-                    <Field type="checkbox" name="rememberMe"/>
-                    <label htmlFor="rememberMe">remember me</label>
+
+                    <Field type="password" name="password" placeholder='password' />
+                    <ErrorMessage name="password" component="div" />
+
+                    <Field type="checkbox" name="rememberMe" /><label htmlFor="rememberMe">remember me</label>
+
+                    <Field name="color" component="select">
+                        <option value="red">Red</option>
+                        <option value="green">Green</option>
+                        <option value="blue">Blue</option>
+                    </Field>
+
+                    <Field name="firstName">
+                        {({ field, form, meta }) => (
+                            <div>
+                                <input type="text" {...field} placeholder="first Name" />
+                                {meta.touched &&
+                                    meta.error && <div className="error">{meta.error}</div>}
+                            </div>
+                        )}
+                    </Field>
+
+                    <Field name="secondName" component={CustomInputComponent} placeholder="Second Name" />
 
                     <button type="submit" disabled={isSubmitting}>
                         Submit
@@ -62,5 +73,18 @@ const LoginForm = () => (
     </div>
 );
 
+const CustomInputComponent = ({
+    field, // { name, value, onChange, onBlur }
+    form: { touched, errors, dirty }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+    ...props
+}) => (
+    <div>
+        <input type="text" {...field} {...props} onClick={() => console.log(dirty)} />
+        {touched[field.name] &&
+            errors[field.name] && <div className="error">{errors[field.name]}</div>}
+    </div>
+);
 
 export default Login;
+
+
