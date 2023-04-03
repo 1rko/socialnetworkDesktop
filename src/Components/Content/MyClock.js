6ece9048c557471e89aab48364ec0132a2214bf3
1rko <117205339+1rko@users.noreply.github.com>
2023-04-03 22:33:22 +0300

    Создала компонент MyClock

---
+++ src/Components/Content/MyClock.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+
+const MyClock = (props) => {
+    let [time, setTime] = useState(0)
+    setInterval(() => {
+        let date = new Date();
+        let hours = date.getHours()
+        let minutes = date.getMinutes()
+        let seconds = date.getSeconds()
+        setTime(hours + " : " + minutes + " : " + seconds)
+        console.log("seconds " + seconds)
+    }, 1000)
+    return (
+        <span> {time}</span>
+    )
+}
+
+export default MyClock;
--- src/Components/Content/Profile/MyProfileInfo/MyProfileInfo.js
+++ src/Components/Content/Profile/MyProfileInfo/MyProfileInfo.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from './MyProfileInfo.module.css'
-import store from "../../../../redux/reduxStore";
 
 class MyProfileInfo extends React.Component {
     state = {
